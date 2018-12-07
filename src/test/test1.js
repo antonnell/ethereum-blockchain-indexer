829d738fd4a21773c93d8d2971a6e3559ab80512
anton <anton@mfin.co.za>
2018-12-07 10:19:11 +0200

    asd
    Signed-off-by: anton <anton@mfin.co.za>

--- src/test/test1.js
+++ src/test/test1.js
@@ -6,5 +6,12 @@ const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8546'
 }))
 
 
-var sync = web3.eth.syncing;
-console.log(sync);
+web3.eth.getSyncing(callback(error, result){
+  if(error) {
+    console.log(error)
+  }
+  if(!error) {
+    console.log(result);
+    console.log(result.highestBlock);
+  }
+})
