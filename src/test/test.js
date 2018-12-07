6d9861d53cd2c3be517484727f318af46f229501
anton <anton@mfin.co.za>
2018-12-07 09:30:15 +0200

    asd
    Signed-off-by: anton <anton@mfin.co.za>

--- src/test/test.js
+++ src/test/test.js
@@ -5,12 +5,57 @@ const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8546'
   }
 }))
 
-var subscription = web3.eth.subscribe(
-  'newBlockHeaders',
-  function (err, blockHeader, sub, existingReceipt) {
-    console.log("blockHeader", blockHeader);
-    subscription.unsubscribe(function() {
-      console.log('Successfully unsubscribed!');
-    });
-  }
-);
+
+var subscription1 = web3.eth.subscribe('pendingTransactions', function(error, result){
+    if (!error)
+        console.log(result);
+})
+.on("data", function(transaction){
+    console.log('transaction')
+    console.log(transaction);
+});
+
+var subscription2 = web3.eth.subscribe('newBlockHeaders', function(error, result){
+    if (!error) {
+        console.log(result);
+
+        return;
+    }
+
+    console.error(error);
+})
+.on("data", function(blockHeader){
+    console.log('block')
+    console.log(blockHeader);
+})
+.on("error", console.error);
+
+var subscription3 = web3.eth.subscribe('syncing', function(error, sync){
+    if (!error)
+        console.log(sync);
+})
+.on("data", function(sync){
+  console.log('syncing')
+    // show some syncing stats
+})
+.on("changed", function(isSyncing){
+    if(isSyncing) {
+        // stop app operation
+    } else {
+        // regain app operation
+    }
+});
+
+var subscription4 = web3.eth.subscribe('logs', {
+    address: '0x123456..',
+    topics: ['0x12345...']
+}, function(error, result){
+    if (!error)
+        console.log(result);
+})
+.on("data", function(log){
+    console.log('logs')
+    console.log(log);
+})
+.on("changed", function(log){
+});
