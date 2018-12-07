f76822a878bf0cb6117c7c23750d341a07d4e73c
anton <anton@mfin.co.za>
2018-12-07 09:24:41 +0200

    a
    Signed-off-by: anton <anton@mfin.co.za>

--- src/test/test.js
+++ src/test/test.js
@@ -1,5 +1,5 @@
 var Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.WebsocketProvider(url, {
+const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8546', {
   headers: {
     Origin: "http://localhost"
   }
