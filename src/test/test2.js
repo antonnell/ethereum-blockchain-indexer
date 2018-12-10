b3734c2a8ac43e6da89c5485bfb3287132a32a3d
anton <anton@mfin.co.za>
2018-12-10 07:28:53 +0200

    a
    Signed-off-by: anton <anton@mfin.co.za>

--- src/test/test2.js
+++ src/test/test2.js
@@ -3,7 +3,20 @@ const net = require('net')
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-console.log(web3.eth.getBlock('latest'))
+web3.eth.getBlock('latest')
+  .then((a, b) => {
+    console.log(a)
+    console.log(b)
+  })
+
 
 const ipcPath = '/root/.ethereum/geth.ipc'
 const web4 = new Web3(new Web3.providers.IpcProvider(ipcPath, net))
+
+
+
+web4.eth.getBlock('latest')
+  .then((a, b) => {
+    console.log(a)
+    console.log(b)
+  })
