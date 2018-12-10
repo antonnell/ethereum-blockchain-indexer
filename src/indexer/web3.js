1240f49e49f7cd6bfc101762f181c9da6c125d7e
anton <anton@mfin.co.za>
2018-12-10 06:19:01 +0200

    using http instead of IPC
    Signed-off-by: anton <anton@mfin.co.za>

--- src/indexer/web3.js
+++ src/indexer/web3.js
@@ -4,6 +4,7 @@ const { enode: { ipcPath } } = require('config')
 const net = require('net')
 const Web3 = require('web3')
 
-const web3 = new Web3(new Web3.providers.IpcProvider(ipcPath, net))
+// const web3 = new Web3(new Web3.providers.IpcProvider(ipcPath, net))
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 module.exports = web3
